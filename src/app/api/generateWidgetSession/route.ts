e0ba60e5376da46690ec92bb73a81e552b78678f
Shiven Patel <shiven110504@gmail.com>
2023-10-20 21:39:42 -0400

    gitignore

--- src/app/api/generateWidgetSession/route.ts
+++ src/app/api/generateWidgetSession/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Terra from "terra-api";
 
-const terra = new Terra(process.env.TERRA_DEV_ID ?? "", process.env.TERRA_API_KEY ?? "", process.env.TERRA_WEBHOOK_SECRET ?? "");
+const terra = new Terra(process.env.TERRA_DEV_ID ?? "universityofmassachusettsamherst-testing-t9yVTx5Fvf", process.env.TERRA_API_KEY ?? "3cCdGaZEjDrEY9Xv9_tjUa34Pttk_b6v", process.env.TERRA_WEBHOOK_SECRET ?? "");
 
 export async function GET(request: NextRequest) {
     const resp = await terra.generateWidgetSession({
