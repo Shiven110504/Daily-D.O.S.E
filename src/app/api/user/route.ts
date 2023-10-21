7a1497a9c8879fba2c937d4eb18724ecc91bf9af
Shino Shirosaki <mjim259@gmail.com>
2023-10-21 00:51:10 -0400

    first uploading endpoints for users

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { mongoClient } from "../config";
+import { ObjectId } from "mongodb";
+
+export async function POST(request: NextRequest) {
+    const data = await request.json()
+    const db = await mongoClient.connect();
+    const userCollect = await db.db('user_data').collection('users').insertOne(data);
+    return NextResponse.json({ data: {
+        success: userCollect.acknowledged
+    } }, { status: 200}); 
+}
+
+export async function GET(request: NextRequest) {
+    let status = 404;
+    let userId;
+    const url = request.url;
+    const responseData = {data : null}
+    const splitUrl = url.split('?');
+    if (splitUrl.length > 0) {
+        const searchParams = new URLSearchParams(splitUrl[1]);
+        userId = searchParams.get('id')
+    }
+    const db = await mongoClient.connect();
+    if (userId) {
+        const userCollect = await db.db('user_data').collection('users').findOne({
+            _id: new ObjectId(userId)
+        });
+        if (userCollect) {
+            responseData.data = userCollect;
+        }
+    } else {
+        status = 400;
+    }
+    
+    return NextResponse.json(responseData, { status: status })
+}
